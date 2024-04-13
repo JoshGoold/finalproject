229d36f54871b9841133d58cb6566db605882dc3
JoshGoold <155192266+JoshGoold@users.noreply.github.com>
2024-04-12 20:29:29 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["*"],
     methods: ["POST", "GET"],
     credentials: true,
   })
